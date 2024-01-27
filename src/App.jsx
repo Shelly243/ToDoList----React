84bfe2e7de4c797f5422267f66a81acda2e0799c
Diya Lohani <shellylohani2018@gmail.com>
2024-01-27 14:49:08 +0530

    modified index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Search from "./components/Search";
 import TodoList from "./components/TodoList";
 import Filter from "./components/Filter";
